224038be403e75a58b19e1633243a37d7e95a758
David Vernon <davidhvernon@mac.com>
2023-04-15 10:17:20 -0600

    mac signing

---
+++ post-build-mac.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -x
+set -e
+cargo build --release
+pushd ./target/release
+if test -f "dirp.zip"; then
+    rm dirp.zip
+fi
+codesign -s  "Developer ID Application: David Vernon (3CT7AJ22D9)" --options=runtime dirp
+zip dirp.zip dirp
+codesign -s  "Developer ID Application: David Vernon (3CT7AJ22D9)" --options=runtime dirp.zip
+xcrun notarytool submit dirp.zip  --keychain-profile "rust-notarize-app" --wait
+popd
