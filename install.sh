54ced82fcadba8915861698a660784737f8ae83a
David Vernon <davidhvernon@mac.com>
2023-04-15 10:19:48 -0600

    install script bug.

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ cd /usr/local/bin
 if test -f "dirp.zip"; then
     rm dirp.zip
 fi
-curl https://raw.githubusercontent.com/DavidHVernon/dirp/master/release/0.1.0/dirp.zip --output cpr.zip
+curl https://raw.githubusercontent.com/DavidHVernon/dirp/master/release/0.1.0/dirp.zip --output dirp.zip
 if test -f "dirp"; then
     rm dirp
 fi
